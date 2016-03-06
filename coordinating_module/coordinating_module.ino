37d85775090162c6020721c5c9f4661b4780e778
Neil Hildick-Smith <Neil Hildick-Smith>
2016-03-05 16:24:19 -0800

    Reabability improvements (including state renaming, general organization, etc)

--- coordinating_module/coordinating_module.ino
+++ coordinating_module/coordinating_module.ino
@@ -2,85 +2,44 @@
 #include <Timers.h>
 
 /*---------------------------PIN Definitions---------------------------------*/
-#define DEPLOYMENT_SERVO_PIN        2   //CONFIRMED (3/3), NHS
+#define DEPLOYMENT_SERVO_PIN        2
 #define MOTOR_ENABLE_RIGHT          3
 #define MOTOR_DIRECTION_RIGHT       12
 #define MOTOR_ENABLE_LEFT           5
 #define MOTOR_DIRECTION_LEFT        6
-#define TOKEN_SENSOR_PIN_0          7   //CONFIRMED (3/3), NHS
-#define TOKEN_SENSOR_PIN_1          9   //CONFIRMED (3/3), NHS
-#define TOKEN_SENSOR_PIN_2          8   //CONFIRMED (3/3), NHS
-#define TOKEN_SENSOR_PIN_3          10  //CONFIRMED (3/3), NHS
+#define TOKEN_SENSOR_PIN_0          7
+#define TOKEN_SENSOR_PIN_1          9
+#define TOKEN_SENSOR_PIN_2          8
+#define TOKEN_SENSOR_PIN_3          10
 #define BUTTON_PIN                  11
 #define LINE_SENSOR_PIN_2           A5
-#define INDICATOR_PIN               13  //CONFIRMED (3/3), NHS
-#define LONG_DISTANCE_SENSOR_PIN_0  A0  //Front-facing
-#define LONG_DISTANCE_SENSOR_PIN_1  A1  //Back-facing
+#define INDICATOR_PIN               13
+#define LONG_DISTANCE_SENSOR_PIN_0  A0
+#define LONG_DISTANCE_SENSOR_PIN_1  A1
 #define SHORT_DISTANCE_SENSOR_PIN   A2
 #define LINE_SENSOR_PIN_0           A3
 #define LINE_SENSOR_PIN_1           A4
 
 /*--------------------------------Definitions----------------------------------*/
-#define TIMER_DEPLOY            9
-#define DEPLOY_PULSE            500
-
-#define TIMER_PIVOT             10
-#define PIVOT_PULSE             340
-
-#define TIMER_OFFSET_PIVOT      15
-#define OFFSET_PIVOT_PULSE      500
-
-#define TIMER_BACKUP            14
-#define BACKUP_PULSE            100
-
-#define DEFAULT_SPEED           69
-#define LINE_SPEED              50
-
-#define SIDE_BIN_DISTANCE       18
-#define FRONT_ORIENT_DISTANCE   120
-#define SIDE_BIN_DISTANCE       12
+#define TIMER_DEPLOY                9
+#define DEPLOY_PULSE                500
+#define TIMER_BACKUP                14
+#define BACKUP_PULSE                100
+#define DRIVE_SPEED                 69
+#define LINE_SPEED                  50
+#define SIDE_BIN_DISTANCE           18
+#define FRONT_ORIENT_DISTANCE       120
 
 /*---------------------------Function prototypes-------------------------------*/
-static void T1_1 (void);
-static void T1_2 (void);
-static void T2_1 (void);
-static void T2_2 (void);
-static void T2_3 (void);
-static void T3_1 (void);
-static void T3_3 (void);
-static void T3_8 (void);
-static void T8_8 (void);
-static void T8_9 (void);
-static void T9_1 (void);
-static void T9_4 (void);
-static void T4_1 (void);
-static void T4_4 (void);
-static void T4_5 (void);
-static void T5_1 (void);
-static void T5_5 (void);
-static void T5_6 (void);
-static void T6_6 (void);
-static void T6_1 (void);
-static void T6_7 (void);
-static void lineFollow (void);
-static bool orientedCorrectly (void);
-static bool binTrigger (void);
-static bool buttonEvent (void);
-static void haltCode (void);
-
 /*---------------------------Constants---------------------------------------*/
 static const uint8_t STATE_1  = 0;
 static const uint8_t STATE_2  = 1;
 static const uint8_t STATE_3  = 2;
 static const uint8_t STATE_4  = 3;
-static const uint8_t STATE_45 = 45;
-static const uint8_t STATE_46 = 46;
-static const uint8_t STATE_47 = 47;
 static const uint8_t STATE_5  = 4;
 static const uint8_t STATE_6  = 5;
 static const uint8_t STATE_7  = 6;
 static const uint8_t STATE_8  = 7;
-static const uint8_t STATE_9  = 8;
 
 /*---------------------------Module Variables--------------------------------*/
 uint8_t state;
@@ -88,8 +47,7 @@ uint8_t front_previous = FRONT_ORIENT_DISTANCE + 1;
 uint8_t side_previous;
 uint8_t button_previous;
 
-/*-----------------------State Machine Coordinator---------------------------*/
-
+/*---------------------------Start-up Sequence-------------------------------*/
 void setup() {
   Serial.begin(9600);
   init_ending_timer();
@@ -104,36 +62,10 @@ void setup() {
   deployment_home();
   start_ending_timer();
   indicator_clear();
-  //init_compass();
-  
-  //the startup sequence
-  //start_up();
 }
 
-#define straight                 0
-#define correct_CW      1
-#define correct_CCW     2
-
-#define CORRECTION        5
-
-uint8_t line_sensing_state = 0;
-
-uint8_t buckets_dropped = 0;
-uint8_t prev_side_bucket_distance = 7;
+/*-----------------------State Machine Coordinator---------------------------*/
 void loop() {
-//  Serial.print("Side: ");
-//  Serial.print(side_distance_sensor());
-//  Serial.print(",   Front: ");
-//  Serial.print(front_distance_sensor());
-//  Serial.print(",   Back: ");
-//  Serial.println(back_distance_sensor());
-//  delay(10);
-//    if (binTrigger()) indicator_pulse();
-//    delay(500);
-//    indicator_clear();
-//drive(-40,-40);
-//deployment_home();
-
   Serial.println(state);
   switch (state) {
     case (STATE_1):
@@ -155,71 +87,24 @@ void loop() {
     break;
     case (STATE_3):
       if (line_under_right()) {
-        T3_8 ();
+        T3_4 ();
       } else if (buttonEvent() | competition_ended()) {
         T3_1 ();
       } else {
         T3_3 ();
       }
     break;
-    case (STATE_8):
-          Serial.println("state 8");
+    case (STATE_4):
       if (TMRArd_IsTimerExpired(TIMER_BACKUP)) {
-        T8_9 ();
+        T4_5 ();
       } else if (buttonEvent() | competition_ended()) {
-        T8_1 ();
-      } else {
-        T8_8 ();
-      }
-    break;
-    case (STATE_9):
-      Serial.println("state 9");
-      if (line_under_rear()) {
-        T9_4 ();
-				
-      } else if (buttonEvent() | competition_ended()) {
-        T9_1 ();
-      } else {
-        T9_9 ();
-      }
-    break;
-    case (STATE_4): //pivot
-      Serial.println("state 4");
-      if (buttonEvent() | competition_ended()) {
         T4_1 ();
-      } else if(line_under_left()){
-        Serial.println("pivot the opposite way");
-        T4_45();
-      }
-      else{
-        T4_45();
-      }
-    break;
-    case (STATE_45):
-      if (!line_under_rear()) {
-        drive (-(LINE_SPEED-8),0);
-      } else if (!line_under_rear() & !line_under_right()) {
-        drive (-(LINE_SPEED-12),0);
       } else {
-        drive (-(LINE_SPEED+1), -(LINE_SPEED+12));
+        T4_4 ();
       }
-      if (binTrigger()) deploy_tokens(1);
     break;
-
-    case (STATE_46):
-        indicator_blanket_set(0, 0,50,0);
-
-    Serial.println("state 46");
-      if(line_under_left()){
-        Serial.println("line under left");
-        T46_45();
-      }
-      break;
-
-    case (STATE_5): //reverse
-          Serial.println("state 5");
-
-      if (TMRArd_IsTimerExpired(TIMER_OFFSET_PIVOT)) {
+    case (STATE_5):
+      if (line_under_rear()) {
         T5_6 ();
       } else if (buttonEvent() | competition_ended()) {
         T5_1 ();
@@ -227,25 +112,37 @@ void loop() {
         T5_5 ();
       }
     break;
-    case (STATE_6): //pivots
-          Serial.println("state 6");
-
-      if (TMRArd_IsTimerExpired(TIMER_OFFSET_PIVOT)) {
+    case (STATE_6):
+      if (line_under_left()){
         T6_7 ();
       } else if (buttonEvent() | competition_ended()) {
         T6_1 ();
-      } else {
-        T6_6 ();
+      } else{
+        T6_7 ();
       }
     break;
+    case (STATE_7):
+      if (binTrigger()) {
+        T7_8();
+      } else if (buttonEvent() | competition_ended()) {
+        T7_1 ();
+      } else {
+        T7_7();
+      }
+    break;
+    case (STATE_8):
+      if (buttonEvent() | competition_ended()) {
+        T8_1 ();
+      } else {
+        T8_7();
+      }
     default:
       state = STATE_1;
     break;
   }
 }
 
-
-/*-----------------------------Helper Functions------------------------------*/
+/*---------------------------Transition Functions----------------------------*/
 static void T1_1 (void) {
   indicator_pulse ();
   state = STATE_1;
@@ -266,7 +163,7 @@ static void T2_1 (void) {
 }
 
 static void T2_2 (void) {
-  drive (DEFAULT_SPEED, -DEFAULT_SPEED);
+  drive (DRIVE_SPEED, -DRIVE_SPEED);
   state = STATE_2;
 }
 
@@ -282,46 +179,13 @@ static void T3_1 (void) {
 }
 
 static void T3_3 (void) {
-  drive(DEFAULT_SPEED, DEFAULT_SPEED);  //temp
+  drive(DRIVE_SPEED, DRIVE_SPEED);  //temp
   state = STATE_3;
 }
 
-static void T3_8 (void) {
+static void T3_4 (void) {
   TMRArd_InitTimer(TIMER_BACKUP, BACKUP_PULSE);
   indicator_blanket_set(0, 0, 50, 50);
-  state = STATE_8;
-}
-
-static void T8_1 (void) {
-  stop_motors ();
-  deployment_home ();
-  state = STATE_1;
-}
-
-static void T8_9 (void) {
-  indicator_blanket_set(10, 10, 10, 10);
-  state = STATE_9;
-}
-
-static void T8_8 (void) {
-  drive(-DEFAULT_SPEED, -DEFAULT_SPEED);
-  state = STATE_8;
-}
-
-static void T9_9 (void) {
-  drive (DEFAULT_SPEED-10, 0);
-  state = STATE_9;
-}
-
-static void T9_1 (void) {
-  stop_motors ();
-  deployment_home ();
-  state = STATE_1;
-}
-
-static void T9_4 (void) {
-  stop_motors();
-  indicator_blanket_set(0, 50, 0, 10);
   state = STATE_4;
 }
 
@@ -331,29 +195,18 @@ static void T4_1 (void) {
   state = STATE_1;
 }
 
+static void T4_5 (void) {
+  indicator_blanket_set(10, 10, 10, 10);
+  state = STATE_5;
+}
+
 static void T4_4 (void) {
-  drive (DEFAULT_SPEED-10, (DEFAULT_SPEED-10));
+  drive(-DRIVE_SPEED, -DRIVE_SPEED);
   state = STATE_4;
 }
 
-static void T4_45(void){
-  drive (-(DEFAULT_SPEED), -(DEFAULT_SPEED-10)); 
-  state = STATE_45;
-}
-
-static void T45_46(void){
-    drive (-(LINE_SPEED +5), -(LINE_SPEED )); 
-    state = STATE_46;
-}
-
-static void T46_45(void){
-  drive (-(LINE_SPEED), -(LINE_SPEED + 5)); 
-  state = STATE_45;
-}
-
-static void T4_5 (void) {
-  TMRArd_InitTimer(TIMER_OFFSET_PIVOT, OFFSET_PIVOT_PULSE);
-  indicator_blanket_set(200, 200, 200, 200);
+static void T5_5 (void) {
+  drive (DRIVE_SPEED-10, 0);
   state = STATE_5;
 }
 
@@ -363,14 +216,9 @@ static void T5_1 (void) {
   state = STATE_1;
 }
 
-static void T5_5 (void) {
-  drive (-(DEFAULT_SPEED-10), -(DEFAULT_SPEED-10));
-  state = STATE_5;
-}
-
 static void T5_6 (void) {
-  TMRArd_InitTimer(TIMER_OFFSET_PIVOT, OFFSET_PIVOT_PULSE);
-  indicator_blanket_set(0, 200, 200, 0);
+  stop_motors();
+  indicator_blanket_set(0, 50, 0, 10);
   state = STATE_6;
 }
 
@@ -381,16 +229,48 @@ static void T6_1 (void) {
 }
 
 static void T6_6 (void) {
-  drive (-(DEFAULT_SPEED-10), DEFAULT_SPEED-10);
+  drive (DRIVE_SPEED-10, (DRIVE_SPEED-10));
   state = STATE_6;
 }
 
-static void T6_7 (void) {
-  //finish me
-  haltCode();
+static void T6_7 (void){
+  drive (-(DRIVE_SPEED), -(DRIVE_SPEED-10)); 
   state = STATE_7;
 }
 
+static void T7_1 (void) {
+  stop_motors ();
+  deployment_home ();
+  state = STATE_1;
+}
+
+static void T7_7 (void) {
+  if (!line_under_rear()) {
+    drive (-(LINE_SPEED-8),0);
+  } else if (!line_under_rear() & !line_under_right()) {
+    drive (-(LINE_SPEED-12),0);
+  } else {
+    drive (-(LINE_SPEED+1), -(LINE_SPEED+12));
+  }
+  state = STATE_7;
+}
+
+static void T7_8 (void) {
+  deploy_tokens(1);
+  state = STATE_8;
+}
+
+static void T8_1 (void) {
+  stop_motors ();
+  deployment_home ();
+  state = STATE_1;
+}
+
+static void T8_7 (void) {
+  state = STATE_7;
+}
+
+/*-----------------------------Helper Functions------------------------------*/
 static bool orientedCorrectly (void) {
   uint8_t front_current = front_distance_sensor();
   if ((front_current > FRONT_ORIENT_DISTANCE) && (front_previous < FRONT_ORIENT_DISTANCE)) {
@@ -416,7 +296,6 @@ static bool buttonEvent (void) {
   uint8_t button_current = button_state();
   if ((button_current == 1) && (button_previous == 0)) {
      button_previous = button_current;
-     
      return true;
   }
   button_previous = button_current;
